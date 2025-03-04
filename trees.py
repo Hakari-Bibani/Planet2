e87a97a86f5c8268c5c51225a0d3f90235c6a797
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-04 14:33:22 +0300

    Update trees.py

--- trees.py
+++ trees.py
@@ -1,62 +1,82 @@
 import streamlit as st
-from handle import run_query, fetch_dataframe
+import pandas as pd
+from handle import run_query, execute_query, fetch_dropdown
 
-def add_tree(data):
-    query = """
-    INSERT INTO "Trees" 
-    ("Common_name", "Scientific_name", "Growth_rate", "Watering_demand", "shape", 
-     "Care_instructions", "Main_Photo_url", "Origin", "Soil_type", "Root_type", "Leafl_Type")
-    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """
-    params = (
-        data.get("Common_name"), data.get("Scientific_name"), data.get("Growth_rate"),
-        data.get("Watering_demand"), data.get("shape"), data.get("Care_instructions"),
-        data.get("Main_Photo_url"), data.get("Origin"), data.get("Soil_type"),
-        data.get("Root_type"), data.get("Leafl_Type")
-    )
-    run_query(query, params)
-
-def get_all_trees():
-    query = 'SELECT * FROM "Trees"'
-    return fetch_dataframe(query)
-
-def tree_data_entry():
-    st.subheader("Tree Data Entry (Single)")
-    with st.form("tree_entry_form"):
+def handle_trees(entry_type):
+    if entry_type == "Single Entry":
+        st.subheader("Add Single Tree")
         common_name = st.text_input("Common Name")
         scientific_name = st.text_input("Scientific Name")
-        growth_rate = st.number_input("Growth Rate", min_value=0.0, format="%.2f")
+        growth_rate = st.number_input("Growth Rate (cm/yr)", value=0.0)
         watering_demand = st.text_input("Watering Demand")
-        
-        # Get existing values for dropdowns if available.
-        df_trees = get_all_trees()
-        shapes = df_trees["shape"].dropna().unique().tolist() if not df_trees.empty else []
-        shape_choice = st.selectbox("Select Existing Shape", options=[""] + shapes)
-        shape = shape_choice if shape_choice != "" else st.text_input("Enter Shape")
-        
+        existing_shape = fetch_dropdown("Trees", "shape")
+        shape_choice = st.selectbox("Shape", (["Add New"] + existing_shape) if existing_shape else ["Add New"])
+        shape = st.text_input("Enter Shape") if shape_choice == "Add New" else shape_choice
+        existing_origin = fetch_dropdown("Trees", "origin")
+        origin_choice = st.selectbox("Origin", (["Add New"] + existing_origin) if existing_origin else ["Add New"])
+        origin = st.text_input("Enter Origin") if origin_choice == "Add New" else origin_choice
+        existing_soil = fetch_dropdown("Trees", "soil_type")
+        soil_choice = st.selectbox("Soil Type", (["Add New"] + existing_soil) if existing_soil else ["Add New"])
+        soil_type = st.text_input("Enter Soil Type") if soil_choice == "Add New" else soil_choice
+        existing_root = fetch_dropdown("Trees", "root_type")
+        root_choice = st.selectbox("Root Type", (["Add New"] + existing_root) if existing_root else ["Add New"])
+        root_type = st.text_input("Enter Root Type") if root_choice == "Add New" else root_choice
+        existing_leaf = fetch_dropdown("Trees", "leafl_type")
+        leaf_choice = st.selectbox("Leaf Type", (["Add New"] + existing_leaf) if existing_leaf else ["Add New"])
+        leafl_type = st.text_input("Enter Leaf Type") if leaf_choice == "Add New" else leaf_choice
         care_instructions = st.text_area("Care Instructions")
         main_photo_url = st.text_input("Main Photo URL")
-        origin = st.text_input("Origin")
-        soil_type = st.text_input("Soil Type")
-        root_type = st.text_input("Root Type")
-        # For Leafl_Type, we use the ENUM values.
-        leaf_types = ['ovate', 'lanceolate', 'linear', 'Deciduous', 'Evergreen']
-        leafl_type = st.selectbox("Leaf Type", options=leaf_types)
-        
-        submitted = st.form_submit_button("Add Tree")
-        if submitted:
-            data = {
-                "Common_name": common_name,
-                "Scientific_name": scientific_name,
-                "Growth_rate": growth_rate,
-                "Watering_demand": watering_demand,
-                "shape": shape,
-                "Care_instructions": care_instructions,
-                "Main_Photo_url": main_photo_url,
-                "Origin": origin,
-                "Soil_type": soil_type,
-                "Root_type": root_type,
-                "Leafl_Type": leafl_type,
-            }
-            add_tree(data)
+        if st.button("Add Tree"):
+            query = """
+            INSERT INTO Trees (common_name, scientific_name, growth_rate, watering_demand, shape, care_instructions, main_photo_url, origin, soil_type, root_type, leafl_type)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+            """
+            execute_query(query, (common_name, scientific_name, growth_rate, watering_demand, shape, care_instructions, main_photo_url, origin, soil_type, root_type, leafl_type))
             st.success("Tree added successfully!")
+    elif entry_type == "Bulk Entry":
+        st.subheader("Bulk Add Trees")
+        file = st.file_uploader("Upload CSV", type=["csv"])
+        if file is not None:
+            df = pd.read_csv(file)
+            for index, row in df.iterrows():
+                query = """
+                INSERT INTO Trees (common_name, scientific_name, growth_rate, watering_demand, shape, care_instructions, main_photo_url, origin, soil_type, root_type, leafl_type)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+                """
+                execute_query(query, (row["common_name"], row["scientific_name"], row["growth_rate"], row["watering_demand"], row["shape"], row["care_instructions"], row["main_photo_url"], row["origin"], row["soil_type"], row["root_type"], row["leafl_type"]))
+            st.success("Bulk trees added!")
+    elif entry_type == "Modify/Delete":
+        st.subheader("Modify/Delete Trees")
+        query = "SELECT * FROM Trees;"
+        data = run_query(query)
+        if data:
+            df = pd.DataFrame(data)
+            st.dataframe(df)
+            selected_id = st.selectbox("Select Tree ID to Modify/Delete", df["tree_id"])
+            action = st.radio("Action", ["Modify", "Delete"])
+            if action == "Delete":
+                if st.button("Delete Tree"):
+                    delete_query = "DELETE FROM Trees WHERE tree_id = %s;"
+                    execute_query(delete_query, (selected_id,))
+                    st.success("Tree deleted!")
+            else:
+                row = df[df["tree_id"] == selected_id].iloc[0]
+                common_name = st.text_input("Common Name", value=row["common_name"])
+                scientific_name = st.text_input("Scientific Name", value=row["scientific_name"])
+                growth_rate = st.number_input("Growth Rate (cm/yr)", value=float(row["growth_rate"]))
+                watering_demand = st.text_input("Watering Demand", value=row["watering_demand"])
+                shape = st.text_input("Shape", value=row["shape"])
+                care_instructions = st.text_area("Care Instructions", value=row["care_instructions"])
+                main_photo_url = st.text_input("Main Photo URL", value=row["main_photo_url"])
+                origin = st.text_input("Origin", value=row["origin"])
+                soil_type = st.text_input("Soil Type", value=row["soil_type"])
+                root_type = st.text_input("Root Type", value=row["root_type"])
+                leafl_type = st.text_input("Leaf Type", value=row["leafl_type"])
+                if st.button("Update Tree"):
+                    update_query = """
+                    UPDATE Trees SET common_name=%s, scientific_name=%s, growth_rate=%s, watering_demand=%s, shape=%s, 
+                    care_instructions=%s, main_photo_url=%s, origin=%s, soil_type=%s, root_type=%s, leafl_type=%s
+                    WHERE tree_id=%s;
+                    """
+                    execute_query(update_query, (common_name, scientific_name, growth_rate, watering_demand, shape, care_instructions, main_photo_url, origin, soil_type, root_type, leafl_type, selected_id))
+                    st.success("Tree updated!")
