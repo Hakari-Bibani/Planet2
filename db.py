ae5a0913d80fae43800eac2421c1f00105f9569c
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-04 14:03:45 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,19 +1,32 @@
 import psycopg2
+from psycopg2.extras import RealDictCursor
 import streamlit as st
 
-@st.experimental_singleton
+CONNECTION_STRING = "postgresql://neondb_owner:npg_YqBVZNepQ18x@ep-orange-bread-a9efjwmt-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+
+@st.cache_resource
 def get_connection():
-    conn = psycopg2.connect("postgresql://neondb_owner:npg_YqBVZNepQ18x@ep-orange-bread-a9efjwmt-pooler.gwc.azure.neon.tech/neondb?sslmode=require")
+    conn = psycopg2.connect(CONNECTION_STRING, cursor_factory=RealDictCursor)
     return conn
 
-def run_query(query, params=None, fetch=True):
+def run_query(query, params=None):
     conn = get_connection()
-    cur = conn.cursor()
-    cur.execute(query, params)
-    if fetch:
-        result = cur.fetchall()
-    else:
-        result = None
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+        try:
+            results = cur.fetchall()
+        except psycopg2.ProgrammingError:
+            results = None
     conn.commit()
-    cur.close()
-    return result
+    return results
+
+def execute_query(query, params=None):
+    conn = get_connection()
+    with conn.cursor() as cur:
+        cur.execute(query, params)
+    conn.commit()
+
+def fetch_dropdown(table, column):
+    query = f"SELECT DISTINCT {column} FROM {table} WHERE {column} IS NOT NULL;"
+    results = run_query(query)
+    return [row[column] for row in results] if results else []
