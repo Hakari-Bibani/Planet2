0cdca9620660975990ee33622d3849ad0b7b6978
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-04 13:55:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,9 @@
 import streamlit as st
-from sidebar import show_sidebar
+from pages import data_entry, search
 
-def main():
-    st.title("Plant Nursery Management App")
-    page = show_sidebar()
-    
-    if page == "Data Entry":
-        st.header("Data Entry")
-        # Choose the table to work on
-        table = st.selectbox("Select Table", ["Nurseries", "Trees", "Nursery_Tree_Inventory"])
-        if table == "Nurseries":
-            from Nurseries import nursery_data_entry
-            nursery_data_entry()
-        elif table == "Trees":
-            from trees import tree_data_entry
-            tree_data_entry()
-        elif table == "Nursery_Tree_Inventory":
-            from Nursery_Tree_Inventory import inventory_data_entry
-            inventory_data_entry()
-    
-    elif page == "Search":
-        st.header("Search Inventory")
-        from research import search_page
-        search_page()
-
-if __name__ == "__main__":
-    main()
+st.set_page_config(page_title="Planet2", layout="wide")
+menu = st.sidebar.radio("Navigation", ["Data Entry", "Search"])
+if menu == "Data Entry":
+    data_entry.app()
+elif menu == "Search":
+    search.app()
