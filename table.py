9567292154eed185cccc9c2e26163454bbc3d459
Hakari Bibani <hakary.jalal33@gmail.com>
2025-03-07 06:28:39 +0300

    Update table.py

--- table.py
+++ table.py
@@ -2,34 +2,21 @@ import streamlit as st
 import nurseries
 import trees
 import inventory
+import payment  # New module for payments
 
 def data_entry_page():
     st.title("Data Entry")
     table_option = st.selectbox("Select Table", ["Nurseries", "Trees", "Nursery_Tree_Inventory", "Payments"])
+    
+    # For Payments, only Modify/Delete is available
     if table_option == "Payments":
-import payment
-    payment.handle_payments("Modify/Delete"))
-
-    tab1, tab2, tab3 = st.tabs(["Single Entry", "Bulk Entry", "Modify/Delete"])
-
-    if table_option == "Nurseries":
-        with tab1:
-            nurseries.handle_nurseries("Single Entry")
-        with tab2:
-            nurseries.handle_nurseries("Bulk Entry")
-        with tab3:
-            nurseries.handle_nurseries("Modify/Delete")
-    elif table_option == "Trees":
-        with tab1:
-            trees.handle_trees("Single Entry")
-        with tab2:
-            trees.handle_trees("Bulk Entry")
-        with tab3:
-            trees.handle_trees("Modify/Delete")
-    elif table_option == "Nursery_Tree_Inventory":
-        with tab1:
-            inventory.handle_inventory("Single Entry")
-        with tab2:
-            inventory.handle_inventory("Bulk Entry")
-        with tab3:
-            inventory.handle_inventory("Modify/Delete")
+        st.info("Payments supports only Modify/Delete functionality.")
+        payment.handle_payments("Modify/Delete")
+    else:
+        entry_type = st.radio("Select Entry Type", ["Single Entry", "Bulk Entry", "Modify/Delete"])
+        if table_option == "Nurseries":
+            nurseries.handle_nurseries(entry_type)
+        elif table_option == "Trees":
+            trees.handle_trees(entry_type)
+        elif table_option == "Nursery_Tree_Inventory":
+            inventory.handle_inventory(entry_type)
